3329f430f9bfa9ecb153d44ed2b2c615fbf8c538
Peter Stimpel <pstimpel+github@gmail.com>
2018-11-25 20:38:07 +0100

    V1.0.9, added GPLv3 disclaimer to code

--- main.inc.php
+++ main.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Version: 1.0.8
+Version: 1.0.9
 Plugin Name: PHP Captcha for Piwigo
 Plugin URI: https://piwigo.org/ext/extension_view.php?eid=882
 Author: pstimpel
@@ -9,7 +9,22 @@ Description: PHP Captcha for Piwigo without using any third party content, track
 */
 
 /**
-	Heavily based on work by Mistic and the PLugin Crypto Captcha
+	Heavily based on work by Mistic and the Plugin Crypto Captcha
+ */
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Chech whether we are indeed included by Piwigo.
